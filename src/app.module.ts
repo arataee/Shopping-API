3b5a942fe0470ad0cce578a314ba06cb05ec3335
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-01 00:10:05 +0330

    feat: add config and sequelize module for root app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import components from './components';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.DB_HOST,
+			port: parseInt(process.env.DB_PORT),
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			autoLoadModels: true,
+			synchronize: true,
+			sync: {},
+		}),
+		GraphQLModule.forRoot({
+			autoSchemaFile: 'schema.gql',
+		}),
+		...components,
+	],
 })
 export class AppModule {}
