aa7615c8b91bcf7a84704ef3a4ca3ee9fca40ce6
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 17:17:19 +0330

    feat(components): add product metas model for sequelize orm and sync in database

---
+++ src/components/product-metas/schemas/product-metas.model.ts
@@ -0,0 +1,43 @@
+import {
+	Table,
+	Column,
+	Model,
+	AutoIncrement,
+	PrimaryKey,
+	DataType,
+	AllowNull,
+	ForeignKey,
+	BelongsTo,
+} from 'sequelize-typescript';
+import { ProductMetaIF } from './product-meta.interface';
+
+/*
+	Association & Relations
+*/
+import { Product } from 'src/components/products/schemas/product.model';
+
+@Table({ updatedAt: false, createdAt: false, tableName: 'product-metas' })
+export class ProductMeta extends Model implements ProductMetaIF {
+	@PrimaryKey
+	@AutoIncrement
+	@Column
+	id: number;
+
+	@AllowNull(false)
+	@Column(DataType.STRING)
+	key: string;
+
+	@Column(DataType.STRING)
+	value: string;
+
+	/*
+		Association & Relations
+	*/
+	@ForeignKey(() => Product)
+	@AllowNull(false)
+	@Column({ type: DataType.INTEGER, field: 'category-id' })
+	product_id: number;
+
+	@BelongsTo(() => Product)
+	product: Product;
+}
