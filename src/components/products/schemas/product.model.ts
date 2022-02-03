4f7a8f22b11008c6e0f9645bd506f4bca38f1955
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 18:05:33 +0330

    fix: add relations between product and other tables

--- src/components/products/schemas/product.model.ts
+++ src/components/products/schemas/product.model.ts
@@ -1,8 +1,75 @@
-import { Table, Column, Model } from 'sequelize-typescript';
+import {
+	Table,
+	Column,
+	Model,
+	AutoIncrement,
+	Unique,
+	PrimaryKey,
+	DataType,
+	BeforeCreate,
+	AllowNull,
+	BelongsToMany,
+	HasMany,
+} from 'sequelize-typescript';
+import slugify from 'slugify';
+import { ProductIF } from './product.interface';
 
-@Table({ updatedAt: false, createdAt: false })
-export class Product extends Model {
+/*
+	Association & Relations
+*/
+import { Category } from '../../categories/schemas/category.model';
+import { Comment } from '../../comments/schemas/comment.model';
+import { Media } from '../../media/schemas/media.model';
+import { ProductCategory } from '../../product-categories/schemas/product-category.model';
+import { ProductMedia } from '../../product-media/schemas/product-media.model';
+import { ProductMeta } from '../../product-metas/schemas/product-metas.model';
+
+@Table({ updatedAt: true, createdAt: true, tableName: 'products' })
+export class Product extends Model implements ProductIF {
+	@PrimaryKey
+	@AutoIncrement
 	@Column
-	name: string;
-	
+	id: number;
+
+	@Column(DataType.STRING)
+	title: string;
+
+	@Column(DataType.STRING)
+	description: string;
+
+	@Column(DataType.TEXT)
+	content: string;
+
+	@Column({ type: DataType.STRING, field: 'index-image' })
+	indexImage: string;
+
+	@BeforeCreate
+	static slugify = (instance: Product) =>
+		(instance.slug = slugify(instance.title));
+
+	@Unique
+	@Column(DataType.STRING)
+	slug: string;
+
+	@Column(DataType.INTEGER)
+	price: number;
+
+	@AllowNull(false)
+	@Column(DataType.INTEGER)
+	stock: number;
+
+	/*
+		Association & Relations
+	*/
+	@BelongsToMany(() => Category, () => ProductCategory)
+	categories: Category[];
+
+	@BelongsToMany(() => Media, () => ProductMedia)
+	media: Media[];
+
+	@HasMany(() => Comment)
+	comments: Comment[];
+
+	@HasMany(() => ProductMeta)
+	productMetas: ProductMeta[];
 }
