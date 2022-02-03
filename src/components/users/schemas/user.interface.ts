264ba50291c0340007de1ac93b67cdbedd16ff83
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 18:06:02 +0330

    fix: add roles enum type for role key

--- src/components/users/schemas/user.interface.ts
+++ src/components/users/schemas/user.interface.ts
@@ -1,9 +1,11 @@
+import { Roles } from './user-roles.enum';
+
 export interface UsersIF {
-    id: number
-    name: string
-    email: string
-    password: string
-    phone: number
-    address: string
-    role: string
+	id: number;
+	name: string;
+	email: string;
+	password: string;
+	phone: number;
+	address: string;
+	role: Roles;
 }
