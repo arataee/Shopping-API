776454663d8a6bc7206068104aff1b00e083c033
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 18:05:23 +0330

    fix: replace title key instead name key

--- src/components/products/schemas/create-product.input.ts
+++ src/components/products/schemas/create-product.input.ts
@@ -1,7 +1,22 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateProductInput {
 	@Field()
-	name: String;
+	title: string;
+
+	@Field()
+	description: string;
+
+	@Field()
+	content: string;
+
+	@Field()
+	indexImage: string;
+
+	@Field(() => Int)
+	price: number;
+
+	@Field(() => Int)
+	stock: number;
 }
