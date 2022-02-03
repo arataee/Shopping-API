bb6b302602f55ccdb2a01a8f950c6d41ac4d6e06
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 17:49:06 +0330

    add all components module

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,19 @@
+import { CategoriesModule } from './categories/categories.module';
+import { CommentsModule } from './comments/comments.module';
+import { MediaModule } from './media/media.module';
+import { ProductCategoriesModule } from './product-categories/product-categories.module';
+import { ProductMediaModule } from './product-media/product-media.module';
+import { ProductMetasModule } from './product-metas/product-metas.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
-export default [ProductsModule];
+export default [
+	CategoriesModule,
+	CommentsModule,
+	MediaModule,
+	ProductCategoriesModule,
+	ProductMediaModule,
+	ProductMetasModule,
+	ProductsModule,
+	UsersModule,
+];
