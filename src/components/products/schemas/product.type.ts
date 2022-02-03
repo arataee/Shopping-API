e4ee2f8fe78559700c71285318c437c060668fe2
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 18:05:43 +0330

    fix: replace title key instead name key

--- src/components/products/schemas/product.type.ts
+++ src/components/products/schemas/product.type.ts
@@ -1,10 +1,29 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ProductIF } from './product.interface';
 
 @ObjectType()
-export class Product {
+export class Product implements ProductIF {
 	@Field(() => Int)
 	id: number;
 
 	@Field()
-	name: String;
+	title: string;
+
+	@Field()
+	description: string;
+
+	@Field()
+	content: string;
+
+	@Field()
+	indexImage: string;
+
+	@Field()
+	slug: string;
+
+	@Field(() => Int)
+	price: number;
+
+	@Field(() => Int)
+	stock: number;
 }
