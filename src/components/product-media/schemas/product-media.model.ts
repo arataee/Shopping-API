5fec0724bd09c138db63ec09a5d60bfecc2a66da
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-03 17:16:40 +0330

    feat(components): add product media model for sequelize orm and sync in database

---
+++ src/components/product-media/schemas/product-media.model.ts
@@ -0,0 +1,31 @@
+import {
+	Table,
+	Column,
+	Model,
+	AutoIncrement,
+	PrimaryKey,
+	DataType,
+	AllowNull,
+	ForeignKey,
+} from 'sequelize-typescript';
+import { Media } from 'src/components/media/schemas/media.model';
+import { Product } from 'src/components/products/schemas/product.model';
+import { ProductMediaIF } from './product-media.interface';
+
+@Table({ updatedAt: false, createdAt: true, tableName: 'products' })
+export class ProductMedia extends Model implements ProductMediaIF {
+	@PrimaryKey
+	@AutoIncrement
+	@Column
+	id: number;
+
+	@ForeignKey(() => Product)
+	@AllowNull(false)
+	@Column({ type: DataType.INTEGER, field: 'product-id' })
+	product_id: number;
+
+	@ForeignKey(() => Media)
+	@AllowNull(false)
+	@Column({ type: DataType.INTEGER, field: 'category-id' })
+	media_id: number;
+}
