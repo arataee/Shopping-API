7c0d5eca7cd6a4d720fa46fa500b2b71cca590c6
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-05 16:41:29 +0330

    feat: add and import jwt module

--- src/components/users/users.module.ts
+++ src/components/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './schemas/user.model';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-	imports: [SequelizeModule.forFeature([User])],
+	imports: [JwtModule.register({}), SequelizeModule.forFeature([User])],
 	providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
