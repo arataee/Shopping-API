f5326940ce144c600a335d22938542a08d143265
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-09 23:35:30 +0330

    feat: add auth module

---
+++ src/components/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+	imports: [PassportModule, JwtModule.register({}), UsersModule],
+	providers: [AuthResolver, AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
