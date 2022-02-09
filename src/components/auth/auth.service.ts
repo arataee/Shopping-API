957f0d8669bbf3233cfab100429c378b1a6b38de
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-09 23:35:13 +0330

    feat: add auth service

---
+++ src/components/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { User } from '../users/schemas/user.model';
+import { UsersService } from '../users/users.service';
+import { RegisterUserInput } from './schemas/register-user.input';
+
+@Injectable()
+export class AuthService {
+	constructor(private users: UsersService, private jwtService: JwtService) {}
+
+	async register(data: RegisterUserInput) {
+		await this.users.create(data);
+	}
+
+	async validateUser(email: string, password: string) {
+		const user = await this.users.findByEmail(email);
+		return user && (await compare(password, user.password)) ? user : null;
+	}
+
+	async login(user: User) {
+		const payload = { id: user.id, role: user.role };
+		const token = this.jwtService.sign(payload, {
+			secret: process.env.AUTH_SECRET,
+			expiresIn: process.env.AUTH_EXPIRES,
+		});
+		return { access_token: token, sub: user.id };
+	}
+}
