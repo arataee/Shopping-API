d87c252f439e38f9bf180ae1a708d8fde2b32aff
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-10 00:23:42 +0330

    feat: remove auth mutations and use auth component

--- src/components/users/users.resolver.ts
+++ src/components/users/users.resolver.ts
@@ -1,16 +1,12 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { UsersService } from './users.service';
-import { User, UserToken } from './schemas/user.type';
+import { User } from './schemas/user.type';
 import { UpdateUserInput } from './schemas/update-user.input';
 import { CreateUserInput } from './schemas/create-user.input';
-import { LoginUserInput } from './schemas/login-user.input';
-import { compare } from 'bcryptjs';
-import { JwtService } from '@nestjs/jwt';
 
 @Resolver()
 export class UsersResolver {
-	constructor(private usersService: UsersService, private jwt: JwtService) {}
-
+	constructor(private usersService: UsersService) {}
 	@Query(() => [User])
 	Users() {
 		return this.usersService.findAll();
@@ -22,6 +18,11 @@ export class UsersResolver {
 	}
 
 	@Mutation(() => User)
+	createUser(@Args('input') input: CreateUserInput) {
+		return this.usersService.create(input);
+	}
+
+	@Mutation(() => User)
 	updateUser(@Args('input') input: UpdateUserInput) {
 		return this.usersService.update(input.id, input);
 	}
@@ -29,28 +30,5 @@ export class UsersResolver {
 	@Mutation(() => User)
 	removeUser(@Args('id', { type: () => Int }) id: number) {
 		return this.usersService.remove(id);
-	}
-
-	@Mutation(() => User)
-	register(@Args('input') input: CreateUserInput) {
-		return this.usersService.create(input);
-	}
-
-	@Mutation(() => UserToken)
-	async login(@Args('input') data: LoginUserInput) {
-		const user = await this.usersService.findByEmail(data.email);
-		if (!user) throw new Error('email or password is incorrect!');
-		const valid = await compare(data.password, user.password);
-		if (!valid) throw new Error('email or password is incorrect!');
-		const { id, role } = user;
-		return {
-			token: await this.jwt.signAsync(
-				{ id, role },
-				{
-					secret: process.env.AUTH_SECRET,
-					expiresIn: process.env.AUTH_EXPIRES,
-				},
-			),
-		};
 	}
 }
