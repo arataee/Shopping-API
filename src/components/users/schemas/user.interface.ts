0a3255fd27d67a7e738703a8cda2fe85095814b6
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-10 22:10:21 +0330

    refactor: remove optional constraint for role key

--- src/components/users/schemas/user.interface.ts
+++ src/components/users/schemas/user.interface.ts
@@ -4,8 +4,8 @@ export interface UsersIF {
 	id: number;
 	name: string;
 	email: string;
-	password?: string;
-	phone: number;
+	password: string;
+	phone: string;
 	address: string;
 	role: Roles;
 }
