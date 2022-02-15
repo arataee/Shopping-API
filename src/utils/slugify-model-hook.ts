35c0ab2b6a7bbd08da9286f40b632575f44df2ec
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-15 21:44:40 +0330

    feat: add slugify model hook in utils, add string trim

---
+++ src/utils/slugify-model-hook.ts
@@ -0,0 +1,18 @@
+import { Model } from 'sequelize-typescript';
+import slugify from 'slugify';
+
+export const slugifyModelKey = <T extends Model>(
+	slugKeyName: keyof T,
+	baseKeyName: keyof T,
+) => {
+	return (instance: T) => {
+		const slugKey = slugKeyName as string,
+			baseKey = baseKeyName as string;
+		if (!instance[slugKey].trim()) {
+			instance[slugKey] = slugify(instance[baseKey]);
+			return;
+		}
+		instance[slugKey] = slugify(instance[slugKey]);
+		return;
+	};
+};
