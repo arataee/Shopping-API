04b162bc87215fb2d13a17b4e1580c3782a8838b
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-17 18:46:44 +0330

    feat: add exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UseAllExceptionFilters } from './utils/exceptions';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalFilters.apply(this, UseAllExceptionFilters);
 	await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
