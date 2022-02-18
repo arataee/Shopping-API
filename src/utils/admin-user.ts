268398d9e3eb4e75e1f8ffa783ca6590e2ab52d5
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-18 16:16:23 +0330

    feat: add default admin user provider

---
+++ src/utils/admin-user.ts
@@ -0,0 +1,26 @@
+import { Provider } from '@nestjs/common';
+import { Role } from '../components/users/schemas/user-roles.enum';
+import { CreateUserInput } from '../components/users/schemas/user.types';
+import { UsersService } from '../components/users/users.service';
+
+export const Admin: Provider = {
+	provide: 'AdminUser',
+	useFactory: async (usersService: UsersService) => {
+		console.log('called');
+		const AdminUser: CreateUserInput = {
+			email: process.env.ADMIN_EMAIL,
+			password: process.env.ADMIN_PASSWORD,
+			role: Role.Admin,
+		};
+		if (!(AdminUser.email && AdminUser.password)) {
+			console.error('Admin user must be configured!');
+			return null;
+		}
+		const user = await usersService.findByEmail(AdminUser.email);
+		if (user) {
+			return await usersService.update(user.id, AdminUser as any);
+		}
+		return await usersService.create(AdminUser as any);
+	},
+	inject: [UsersService],
+};
