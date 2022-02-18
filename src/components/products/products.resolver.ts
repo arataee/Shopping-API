9a27ad242a14b799c0b3d20f13f2b54edab94aac
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-18 16:39:36 +0330

    refactor: error handling for methods

--- src/components/products/products.resolver.ts
+++ src/components/products/products.resolver.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { ProductsService } from './products.service';
 import {
@@ -17,7 +18,11 @@ export class ProductsResolver {
 
 	@Query(() => Product, { nullable: true })
 	Product(@Args('id', { type: () => Int }) id: number) {
-		return this.productsService.findOne(id);
+		const product = this.productsService.findOne(id);
+		if (!product) {
+			throw new NotFoundException();
+		}
+		return product;
 	}
 
 	@Mutation(() => Product)
@@ -27,11 +32,19 @@ export class ProductsResolver {
 
 	@Mutation(() => Product)
 	updateProduct(@Args('input') input: UpdateProductInput) {
-		return this.productsService.update(input.id, input);
+		const product = this.productsService.update(input.id, input);
+		if (!product) {
+			throw new NotFoundException();
+		}
+		return product;
 	}
 
 	@Mutation(() => Product)
 	removeProduct(@Args('id', { type: () => Int }) id: number) {
-		return this.productsService.remove(id);
+		const product = this.productsService.remove(id);
+		if (!product) {
+			throw new NotFoundException();
+		}
+		return product;
 	}
 }
