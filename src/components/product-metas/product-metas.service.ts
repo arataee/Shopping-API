4f4b89bedad4b6ad245f0a44db327a2ba629ed2f
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-02-18 16:52:36 +0330

    refactor: return null for methods

--- src/components/product-metas/product-metas.service.ts
+++ src/components/product-metas/product-metas.service.ts
@@ -27,16 +27,22 @@ export class ProductMetasService {
 	}
 
 	async create(data: CreateProductMetaInput) {
-		return await this.productMeta.create({ ...data });
+		return await this.productMeta.create(data as any);
 	}
 
 	async update(id: number, data: UpdateProductMetaInput) {
 		const productMeta = await this.findOne(id);
+		if (!productMeta) {
+			return null;
+		}
 		return await productMeta.update(data);
 	}
 
 	async remove(id: number) {
 		const productMeta = await this.findOne(id);
+		if (!productMeta) {
+			return null;
+		}
 		await productMeta.destroy();
 		return productMeta;
 	}
