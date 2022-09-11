c1ad9f0c952899a5937d4cc1fe1511c2c1c59771
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-09-11 12:50:46 +0430

    up

--- test/e2e/queries.ts
+++ test/e2e/queries.ts
@@ -1,5 +1,44 @@
+// import * as Auth from './queries/auth';
+// import * as Categories from './queries/categories';
+// import * as Comments from './queries/comments';
+// import * as Media from './queries/media';
+// import * as ProductCategories from './queries/product-categories';
+// import * as ProductMedia from './queries/product-media';
+// import * as ProductMetas from './queries/product-metas';
+// import * as Products from './queries/products';
+import {
+	user_create_auth,
+	user_create,
+	user_create_uniqueEmail,
+	user_create_validation,
+	user_delete,
+	user_delete_auth,
+	user_delete_notFound,
+	user_find,
+	user_findAll,
+	user_find_notFound,
+	user_update,
+	user_update_auth,
+	user_update_notFound,
+	user_update_validation,
+} from './queries/users';
 import { Queries } from './utils';
 
-const queries: Queries[] = [];
+const queries: Queries[] = [
+	user_create_auth,
+	user_create_validation,
+	user_create_uniqueEmail,
+	user_create,
+	// user_find,
+	// user_find_notFound,
+	// user_findAll,
+	// user_update_auth,
+	// user_update_notFound,
+	// user_update_validation,
+	// user_update,
+	// user_delete_auth,
+	// user_delete_notFound,
+	// user_delete,
+];
 
 export default queries;
--- test/e2e/queries/users.ts
+++ test/e2e/queries/users.ts
@@ -1,1 +1,142 @@
 import { Queries } from '../utils';
+
+const fragment = `{ id name email phone address role }`;
+const input = `{ name: "user" email: "test@email.com" password: "A123456z" phone: "+989171234567" address: "test" role: User }`;
+
+// Create
+export const user_create_auth: Queries = {
+	title: 'Users > Create > Auth Error',
+	expectQuery: `mutation { createUser( input: ${input} ) ${fragment} }`,
+	expectData: {
+		errors: [expect.anything()],
+		data: null,
+	},
+};
+export const user_create_validation = {
+	title: 'Users > Create > Validation Error',
+	expectQuery: '',
+	expectData: { errors: {}, data: {} },
+	auth: true,
+};
+export const user_create_uniqueEmail = {
+	title: 'Users > Create > Unique Email Error',
+	expectQuery: '',
+	expectData: { errors: {}, data: {} },
+	auth: true,
+};
+export const user_create: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+
+// Find
+export const user_find: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_find_notFound: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_findAll: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+
+// Update
+export const user_update_auth: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_update_notFound: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_update_validation: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_update: Queries = {
+	title: 'Users > Create > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+
+// Delete
+export const user_delete_auth: Queries = {
+	title: 'Users > Delete > Auth Error',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_delete_notFound: Queries = {
+	title: 'Users > Delete > Not Found Error',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
+export const user_delete: Queries = {
+	title: 'Users > Delete > OK',
+	expectQuery: user_create_auth.expectQuery,
+	expectData: {
+		data: {
+			createUser: expect.anything(),
+		},
+	},
+	auth: true,
+};
